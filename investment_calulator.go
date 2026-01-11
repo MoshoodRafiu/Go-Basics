b579b80cdabe06b166192a9a3c7af2f0f783a02a
Rafiu Moshood <meezyloaded@gmail.com>
2026-01-11 14:21:18 +0100

    learn: working with functions

--- investment_calulator.go
+++ investment_calulator.go
@@ -5,26 +5,39 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
 	var investmentAmount float64
 	var years float64
 	expectedReturnRate := 5.5
 
-	fmt.Print("Investment Amount: ")
+	outputText("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
 
-
-	fmt.Print("Expected Return Rate: ")
+	outputText("Expected Return Rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	fmt.Print("Years: ")
+	outputText("Years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureValue, futureRealValue := calculateFutureValue(investmentAmount, expectedReturnRate, years);
+	// futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	// fmt.Println("Future Value: ", futureValue)
+	fmt.Printf("Future Value: %.1f\nFuture Value (Adjusted for Inflation): %.1f", futureValue, futureRealValue);
+	// fmt.Println("", futureRealValue)
+}
+
+func outputText(text string) {
+	fmt.Print(text);
+}
+
+func calculateFutureValue(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+
+	return fv, rfv
 }
