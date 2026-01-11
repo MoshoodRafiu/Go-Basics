6a743ec427dea5119d214b64418f12993883c825
Rafiu Moshood <meezyloaded@gmail.com>
2026-01-11 13:49:00 +0100

    learn: working with user inputs

--- investment_calulator.go
+++ investment_calulator.go
@@ -6,11 +6,25 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000
-	var expectedReturnRate float64 = 5.5
-	var years float64 = 10
+	const inflationRate = 2.5
+	var investmentAmount float64
+	var years float64
+	expectedReturnRate := 5.5
 
-	var futureValue float64 = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
 
-	fmt.Print((futureValue))
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
